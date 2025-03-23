460fdb5326c4c1a7caa0d34f265c525221465231
Khurshidbek-Ethan <ethankhurshidbek@gmail.com>
2025-03-23 12:25:52 +0900

    Redux and React intagrated with Hooks, useSelector and useDispatch

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,23 +1,90 @@
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import * as actions from '../redux/actions'
+// import { connect } from 'react-redux'
+// import { bindActionCreators } from 'redux'
+// import * as actions from '../redux/actions'
+// const Counter = ({ counter, inc, decr, rnd }) => {
+// 	return (
+// 		<div className='w-full vh-100 bg-dark text-white d-flex justify-content-center align-items-center'>
+// 			<div className='border border-5 w-50 p-5 rounded border-success border-opacity-50 d-flex align-items-center flex-column bg-secondary'>
+// 				<h1>
+// 					Counter: <span>{counter}</span>
+// 				</h1>
 
-const Counter = ({ counter, inc, decr, rnd }) => {
+// 				<div className='d-flex align-items-center gap-2'>
+// 					<button className='btn btn-success' onClick={inc}>
+// 						Increment
+// 					</button>
+// 					<button className='btn btn-danger' onClick={decr}>
+// 						Decrement
+// 					</button>
+// 					<button className='btn btn-info' onClick={rnd}>
+// 						Random
+// 					</button>
+// 				</div>
+// 			</div>
+// 		</div>
+// 	)
+// }
+
+// const mapStateToProps = state => {
+// 	return {
+// 		counter: state.count,
+// 	}
+// }
+
+// const mapDispatchToProps = dispatch => {
+// 	return bindActionCreators(actions, dispatch)
+// 	// const { inc, decr, rnd } = bindActionCreators(actions, dispatch)
+// 	// return {
+// 	// 	inc,
+// 	// 	decr,
+// 	// 	rnd,
+// 	// 	// inc: () => dispatch({ type: 'INC' }),
+// 	// 	// decr: () => dispatch({ type: 'DECR' }),
+// 	// 	// rnd: () => {
+// 	// 	// 	const randomValue = Math.floor(Math.random() * 100)
+// 	// 	// 	dispatch({ type: 'RND', payload: randomValue })
+// 	// 	// },
+// 	// }
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter)
+
+/////////////////////////   redux and react intagratsiya hooks   ///////////////////////////////////////
+
+import { useDispatch, useSelector } from 'react-redux'
+import { inc, decr, rnd } from '../redux/actions'
+
+const Counter = () => {
+	// reducerni ichidagi statetimizni qaytarib beradi
+	const { count } = useSelector(state => state)
+
+	// reducerlarni ozgartiradigon dispatch
+	const dispatch = useDispatch()
+
 	return (
 		<div className='w-full vh-100 bg-dark text-white d-flex justify-content-center align-items-center'>
 			<div className='border border-5 w-50 p-5 rounded border-success border-opacity-50 d-flex align-items-center flex-column bg-secondary'>
 				<h1>
-					Counter: <span>{counter}</span>
+					Counter: <span>{count}</span>
 				</h1>
 
 				<div className='d-flex align-items-center gap-2'>
-					<button className='btn btn-success' onClick={inc}>
+					<button
+						className='btn btn-success'
+						onClick={() => dispatch(inc())}
+					>
 						Increment
 					</button>
-					<button className='btn btn-danger' onClick={decr}>
+					<button
+						className='btn btn-danger'
+						onClick={() => dispatch(decr())}
+					>
 						Decrement
 					</button>
-					<button className='btn btn-info' onClick={rnd}>
+					<button
+						className='btn btn-info'
+						onClick={() => dispatch(rnd())}
+					>
 						Random
 					</button>
 				</div>
@@ -26,26 +93,4 @@ const Counter = ({ counter, inc, decr, rnd }) => {
 	)
 }
 
-const mapStateToProps = state => {
-	return {
-		counter: state.count,
-	}
-}
-
-const mapDispatchToProps = dispatch => {
-	return bindActionCreators(actions, dispatch)
-	// const { inc, decr, rnd } = bindActionCreators(actions, dispatch)
-	// return {
-	// 	inc,
-	// 	decr,
-	// 	rnd,
-	// 	// inc: () => dispatch({ type: 'INC' }),
-	// 	// decr: () => dispatch({ type: 'DECR' }),
-	// 	// rnd: () => {
-	// 	// 	const randomValue = Math.floor(Math.random() * 100)
-	// 	// 	dispatch({ type: 'RND', payload: randomValue })
-	// 	// },
-	// }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Counter)
+export default Counter
--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Provider } from 'react-redux'
 import reducer from './redux/reducer.js'
-import { legacy_createStore } from 'redux'
+import { legacy_createStore, compose, applyMiddleware } from 'redux'
 import App from './components/app.jsx'
 
-const store = legacy_createStore(reducer)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = legacy_createStore(reducer, composeEnhancers(applyMiddleware()))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<StrictMode>
 		<Provider store={store}>
 			<App />
 		</Provider>
-		,
 	</StrictMode>
 )
